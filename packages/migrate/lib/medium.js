a85538c355058f5a4ee0b60ca1f5768a686aba64
Hannah Wolfe <erisds@gmail.com>
2018-10-24 15:38:41 +0100

    ✨ Add minimal scraper tool
    - Requires async behaviour, used async await to keep syntax super clean
    - Add the ability to scrape to hydrate our data with anything that is missing
    - This tool works in the pre Ghost JSON format of { resource: [{ url: 'somat', data: { foo: bar } }] }
    - Using (currently) the format supported by Scrape It: https://www.npmjs.com/package/scrape-it, it's possible to spec extra data to fetch
    - The merging tools do a naive job of getting the format right

--- packages/migrate/lib/medium.js
+++ packages/migrate/lib/medium.js
@@ -1,7 +1,28 @@
 const mediumIngest = require('@tryghost/mg-medium-export');
 const mgJSON = require('@tryghost/mg-json');
+const MgScraper = require('@tryghost/mg-webscraper');
 const converter = require('@tryghost/html-to-mobiledoc');
 const fs = require('./fs');
+
+const scrapeConfig = {
+    posts: {
+        tags: {
+            listItem: 'ul.tags > li',
+            data: {
+                url: {
+                    selector: 'a',
+                    attr: 'href'
+                },
+                // @TODO ideally we'd spec this using a data key, so the structure reflects what we expect back
+                name: {
+                    selector: 'a'
+                }
+            }
+        }
+    }
+};
+
+const mediumScraper = new MgScraper(scrapeConfig);
 
 /**
  * Migrate from Medium
@@ -11,11 +32,12 @@ const fs = require('./fs');
  * @param {String} pathToZip
  * @param {Boolean} verbose
  */
-module.exports.migrate = (pathToZip) => {
+module.exports.migrate = async (pathToZip) => {
     // 1. Read the zip file
     let result = mediumIngest(pathToZip);
 
     // 2. Pass the results through the web scraper to get any missing data
+    result = await mediumScraper.hydrate(result);
 
     // 3. Format the data as a valid Ghost JSON file
     result = mgJSON.toGhostJSON(result);
